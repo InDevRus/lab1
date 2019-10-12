bfdb6d267be74beae5abb12817e0206442b576f8
InDev <InDevRus@hotmail.com>
2019-10-12 19:15:41 +0500

    Reduced button code

--- musicbox/button.h
+++ musicbox/button.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#define BUTTON_DEBOUNCE_DELAY 50
-
 class Button
 {
 public:
@@ -9,39 +7,17 @@ public:
     {
         pin = _pin;
         state = LOW;
-        debounceState = LOW;
 
         pinMode(pin, INPUT_PULLUP);
     }
 
     bool wasPressed()
     {
-        bool result = false;
-        int lastSeen = state;
-        readButtonWithDebounce();
-
-        if (lastSeen == HIGH && state == LOW)
-            result = true;
-
-        return result;
-    }
-
-    void readButtonWithDebounce()
-    {  
-        int current = digitalRead(pin);
-
-        if (current != debounceState)
-            debounceLastMs = millis();
-
-        if ((millis() - debounceLastMs) > BUTTON_DEBOUNCE_DELAY)
-            state = current;
-
-        debounceState = current;
+        state = digitalRead(pin);
+        return state == LOW;
     }
 
 private:
     int pin;
     int state;
-    int debounceState;
-    unsigned long debounceLastMs;
 };
